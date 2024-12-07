401c2a5850c8ee7b812001ac3af94e902196fba7
diegomartine <martinezsierradiegoferney@gmail.com>
2024-12-06 22:04:59 -0500

    update

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/debug', function () {
+    return response()->json([
+        'APP_URL' => env('APP_URL'),
+        'PORT' => env('PORT'),
+        'DB_HOST' => env('DB_HOST'),
+        'DB_PORT' => env('DB_PORT'),
+        'DB_DATABASE' => env('DB_DATABASE'),
+        'DB_USERNAME' => env('DB_USERNAME'),
+    ]);
 });
